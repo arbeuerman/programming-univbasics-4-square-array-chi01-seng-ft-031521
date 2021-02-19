ad35ab46d0ea5600e03d5f10eb7ec45add38c028
Alexandra Rodriguez-Beuerman <arbeuerman@gmail.com>
2021-02-19 19:00:18 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,4 +1,9 @@
 def square_array(array)
   array_length = array.length 
-  length.times 
+  squared_array = []
+  length.times do |index|
+    new_element = array[index]**2
+    squared_array.push new_element
+  end 
+  squared_array
 end
